18349079e3ba47b46602edf0145d1bbe2188802b
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-20 20:40:06 -0300

    Criado a classe funcionario com seu construtor e regra de negócio. Também foi criado a lista e usado Add instanciando na classe. Impressão do dado.

---
+++ ExercicioListaV2/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioListaV2
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double porcentagem)
+        {
+            //Regra para aumento salarial com entrada em porcentagem
+            Salario = Salario + (Salario * (porcentagem / 100));
+        }
+
+        //public void Lista ()
+
+        //Sobreposição
+        public override string ToString()
+        {
+            return "Id: " + Id
+                 + "\n"
+                 + "Name: " + Nome
+                 + "\n"
+                 + "Salary: " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- ExercicioListaV2/Program.cs
+++ ExercicioListaV2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace ExercicioListaV2
@@ -7,7 +8,46 @@ namespace ExercicioListaV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int id;
+            string nm;
+            double sal;
+            int posicao = 0;
+
+            Console.Write("Quando funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Declaração da lista
+            List<Funcionario> ListaFuncionario = new List<Funcionario>();
+
+            if(n > 0)
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    Console.Write("ID: ");
+                    id = int.Parse(Console.ReadLine());
+                    Console.Write("Nome: ");
+                    nm = Console.ReadLine();
+                    Console.WriteLine("Salario: ");
+                    sal = double.Parse(Console.ReadLine());
+
+                    //Adicionando item na lista e instanciando usando o construtor da classe
+                    ListaFuncionario.Add(new Funcionario(id, nm, sal));                   
+
+                }
+
+                //Impressao dos dados               
+                foreach(Funcionario f in ListaFuncionario)
+                {
+                    posicao = posicao + 1; //Para mostrar de forma dinâmica a posição do funcionario
+                    Console.WriteLine("Employee #" + posicao + ":"
+                                     + "\n"
+                                     + f);                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum funcionário registrado.");
+            }
         }
     }
 }
